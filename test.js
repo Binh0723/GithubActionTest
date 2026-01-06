c59a1e853ff887f720b80d994fe3ebb8c35c80b5
Binh0723 <binhtrancongduc2307@gmail.com>
2026-01-06 00:10:23 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+// Simulating a simple test
+const sum = 1 + 1;
+if (sum === 2) {
+  console.log("Test Passed!");
+  process.exit(0); // Success code
+} else {
+  console.log("Test Failed!");
+  process.exit(1); // Error code
+}
